666ba1bc8cff80a5353357898fe1ffaa05626c17
chandni881 <cpatel4@umbc.edu>
2017-02-17 21:33:32 +0000

    Anagram Strings

--- src/main/java/First/App.java
+++ src/main/java/First/App.java
@@ -1,5 +1,5 @@
 package First;
-
+import java.util.Arrays;
 /**
  * Hello world!
  *
@@ -8,24 +8,35 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
-        System.out.println("The result is:" +isRotation("hello", "ohell")); 
-        System.out.println("The result is:" +isRotation(null,null));
-        System.out.println("The result is:" +isRotation(null,"chandni"));
-        System.out.println("The result is:" +isRotation("Gaurang","Chandni"));        
+        System.out.println("The result is:" +isAnagram("hello", "ohell")); 
+        System.out.println("The result is:" +isAnagram(null,null));
+        System.out.println("The result is:" +isAnagram(null,"chandni"));
+        System.out.println("The result is:" +isAnagram("Gaurangii","Chandni"));        
+        System.out.println("The result is:" +isAnagram("abcd","dacb"));
     }
 
-private static boolean isRotation (String one, String two){
+private static boolean isAnagram (String one, String two){
+    boolean ana = false;
     if(one==null && two==null){
-        return true;
+       return true;
     }
     if(one==null || two==null){
         return false;
     }
     if(one.length()!=two.length()){
-        return false;
+       ana = false;
     }
-    String result=one+one;
-    return result.contains(two); 
+    else{
+        char[] s1= one.toCharArray();
+        char[] s2= two.toCharArray();
+         Arrays.sort(s1);
+         Arrays.sort(s2);
+            if (Arrays.equals(s1,s2)){
+                 ana = true;
+            }
+ }
+
+     return ana;
+     
 }
 }
