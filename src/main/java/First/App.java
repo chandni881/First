fdf7e5e4a0a522672f08ba4c17124869af166e04
chandni881 <cpatel4@umbc.edu>
2017-02-18 00:40:00 +0000

    Anagrams using HashMaps

--- src/main/java/First/App.java
+++ src/main/java/First/App.java
@@ -1,5 +1,6 @@
 package First;
 import java.util.Arrays;
+import java.util.HashMap;
 /**
  * Hello world!
  *
@@ -13,9 +14,11 @@ public class App
         System.out.println("The result is:" +isAnagram(null,"chandni"));
         System.out.println("The result is:" +isAnagram("Gaurangii","Chandni"));        
         System.out.println("The result is:" +isAnagram("abcd","dacb"));
+        System.out.println("The result is:" +isAnagram(" "," "));
+        
     }
 
-private static boolean isAnagram (String one, String two){
+/*private static boolean isAnagram (String one, String two){
     boolean ana = false;
     if(one==null && two==null){
        return true;
@@ -38,5 +41,41 @@ private static boolean isAnagram (String one, String two){
 
      return ana;
      
+}*/
+private static boolean isAnagram (String one, String two){
+
+    if(one==null && two==null){
+       return true;
+    }
+    if(one==null || two==null){
+        return false;
+    }
+    if(one.length()!=two.length()){
+       return false;
+    }
+    
+    HashMap<Character, Integer> hmap1 = new HashMap<Character, Integer>();
+    HashMap<Character, Integer> hmap2 = new HashMap<Character, Integer>();
+    
+    hmap1=makeItAHashMap(one);
+    hmap2=makeItAHashMap(two);
+    
+    if(hmap1.hashCode()==hmap2.hashCode()){
+        return true;
+    }
+    return false;
+   
+   }
+
+private static HashMap<Character, Integer> makeItAHashMap(String str){
+     HashMap<Character, Integer> hmap = new HashMap<Character, Integer>();
+    for(int i=0; i<str.length();i++){
+        if(!hmap.containsKey(str.charAt(i))){
+            hmap.put(str.charAt(i), 0);
+        }
+        hmap.put(str.charAt(i), hmap.get(str.charAt(i))+1);
+    }
+    return hmap;
 }
+
 }
