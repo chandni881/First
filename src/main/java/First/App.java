b0c5b2f9eab4ff6772148b723a5566c5bb10f22d
chandni881 <cpatel4@umbc.edu>
2017-02-21 21:04:16 +0000

    Palindrome with multiple words

--- src/main/java/First/App.java
+++ src/main/java/First/App.java
@@ -12,17 +12,21 @@ public class App
         System.out.println("The result is:" +isPalindrome("mom")); 
         System.out.println("The result is:" +isPalindrome(null));
         System.out.println("The result is:" +isPalindrome("aa"));
-        System.out.println("The result is:" +isPalindrome("aabc"));        
+        System.out.println("The result is:" +isPalindrome("aabc"));   
+        System.out.println("The result is:" +isPalindrome("my gym"));
     }
 
-private static boolean isPalindrome (String str){
-    if(str==null){
+private static boolean isPalindrome (String string){
+    if(string==null){
         return true;
     }
-    if(str.isEmpty()){
+    if(string.isEmpty()){
         return true;
     }
+    String str = string.replace(" ","");
+    System.out.println(str);
     for (int i=0;i<(str.length())/2 ; i++){
+        
         if(str.charAt(i)!=str.charAt((str.length()-1)-i)){
             return false;
         }
